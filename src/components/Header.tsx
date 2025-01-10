458691a8e5ca66659b5a3389ac153e5edf5035af
AkashJana18 <akashjana663@gmail.com>
2025-01-10 19:25:43 +0530

    UI: Header improvement

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import { IconBrandGithub, IconBrandGithubFilled } from "@tabler/icons-react";
+import Link from "next/link";
+import { useState } from "react";
 
 export function Header() {
   const words = [
@@ -12,9 +15,26 @@ export function Header() {
       className: "text-teal-500 dark:text-teal-300",
     },
   ];
+
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="absolute top-2">
+    <div className="absolute top-2 flex justify-between w-full px-4 items-center">
       <TypewriterEffectSmooth words={words} />
+      <p className="text-2xl font-thin tracking-widest">Built by Akash with CopilotKit.ai🪁 &#169;</p>
+      <Link
+        href="https://github.com/AkashJana18/blogai"
+        target="blank"
+        className="flex cursor-pointer"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {!isHovered ? (
+          <IconBrandGithub size={46} />
+        ) : (
+          <IconBrandGithubFilled size={46} />
+        )}
+      </Link>
     </div>
   );
 }
